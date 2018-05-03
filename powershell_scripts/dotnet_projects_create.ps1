bb1cbe6f85915da265290c8a0ca84913203dbda1
Unknown <raxp@hotmail.co.uk>
2018-05-03 13:38:59 +0100

    tried to add project reference netstandard2.0 --> net461 link
    this returns an error
    
     cannot be added due to 
    incompatible targeted frameworks between the two projects. Please review the project you are trying to add and verify that is compatible with the following targets:
    At line:1 char:1
    
    
    Works if we edit classlib_net461.csproj and add multiple targets including netstandard2.0
    
     <PropertyGroup>
        <TargetFrameworks>net461;netstandard2.0</TargetFrameworks>
      </PropertyGroup>
    
    
    #>
    
    dotnet add .\classlib_netstandard2.0_ref_classLib_net461\classlib_netstandard2.0_ref_classLib_net461.csproj reference .\classlib_net461\classlib_net461.csproj

--- powershell_scripts/dotnet_projects_create.ps1
+++ powershell_scripts/dotnet_projects_create.ps1
@@ -4,3 +4,25 @@ dotnet new classlib --name classlib_net461 --target-framework-override net461
 
 # netstandard2.0 --> net461 link
 dotnet new classlib --name classlib_netstandard2.0_ref_classLib_net461 --framework netstandard2.0
+
+<#
+add project a reference 
+netstandard2.0 --> net461 link
+
+this returns an error
+
+ cannot be added due to 
+incompatible targeted frameworks between the two projects. Please review the project you are trying to add and verify that is compatible with the following targets:
+At line:1 char:1
+
+
+Works if we edit classlib_net461.csproj and add multiple targets including netstandard2.0
+
+ <PropertyGroup>
+    <TargetFrameworks>net461;netstandard2.0</TargetFrameworks>
+  </PropertyGroup>
+
+
+#>
+
+dotnet add .\classlib_netstandard2.0_ref_classLib_net461\classlib_netstandard2.0_ref_classLib_net461.csproj reference .\classlib_net461\classlib_net461.csproj
